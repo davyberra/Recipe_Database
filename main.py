a4fe407b17acd171b19c56d9e960491a0f78755b
unknown <davyberra@gmail.com>
2021-01-24 14:12:24 -0800

    Created rudimentary table and added (most) recipes

---
+++ main.py
@@ -0,0 +1,66 @@
+import mysql.connector
+from mysql.connector import Error
+from puller import *
+from sql_query_functions import *
+
+DB = "recipe_database"
+
+create_recipe_table = """
+CREATE TABLE Recipe (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(80)
+ );
+"""
+
+create_ingredient_table = """
+CREATE TABLE Ingredient (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+"""
+
+create_recipe_ingredient_table = """
+CREATE TABLE RecipeIngredient (
+    recipe_id INT NOT NULL,
+    ingredient_id INT NOT NULL
+);
+"""
+
+alter_recipe_ingredient_table = """
+ALTER TABLE recipeingredient
+ADD FOREIGN KEY(recipe_id)
+REFERENCES Recipe(id);
+"""
+
+alter_recipe_ingredient_table_again = """
+ALTER TABLE RecipeIngredient
+ADD FOREIGN KEY(ingredient_id)
+REFERENCES Ingredient(id);
+"""
+
+add_recipe = """
+INSERT INTO recipe (name)
+VALUES ('Paneer')
+"""
+
+
+def add_recipes(recipe_list, c):
+    for recipe in recipe_list:
+        for name in recipe.keys():
+            try:
+                query = f"""
+                INSERT INTO recipe (name)
+                VALUES ('{name}')
+                """
+                execute_query(c, query)
+            except Error as e:
+                print(f"The error '{e}' for recipe '{name}' has occured.")
+
+def main():
+    connection = create_connection(HOST_NAME, USER_NAME, PASSWORD, DB)
+
+    recipe_list = pull_ingredients('recipes')
+    add_recipes(recipe_list, connection)
+
+if __name__ == '__main__':
+    main()
--- puller.py
+++ puller.py
@@ -1,1 +1,35 @@
 from bs4 import BeautifulSoup
+import os
+from pathlib import Path
+from mysql.connector import Error
+
+
+
+def pull_ingredients(folder):
+    recipe_folder = Path.cwd() / folder
+    recipe_list = []
+    for file in os.listdir(recipe_folder):
+
+        recipe_path = recipe_folder / Path(file)
+        recipe = open(recipe_path)
+        try:
+            soup = BeautifulSoup(recipe, 'html.parser')
+            ingredients = soup.select('p[itemprop="recipeIngredient"]')
+            ingredient_list = {}
+            ingredient_list[recipe_path.stem] = []
+            for ingredient in ingredients:
+                ingredient_list[recipe_path.stem].append(ingredient.text)
+            recipe_list.append(ingredient_list)
+            recipe.close()
+        except:
+            print(f"Uh-oh. A problem occurred with '{recipe_path.stem}'.")
+
+
+    return recipe_list
+
+
+def main():
+    print(pull_ingredients('recipes'))
+
+if __name__ == '__main__':
+    main()
